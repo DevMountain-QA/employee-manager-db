eb7faeeacc4585ccd774bf5d46aa688768c189a9
AJ Larson <aj.larson@devmounta.in>
2018-03-27 01:03:10 -0600

    updated

--- server/index.js
+++ server/index.js
@@ -19,4 +19,4 @@ app.put('/api/employees/:id', ec.update)
 app.delete('/api/employees/:id', ec.delete)
 
 const port = process.env.PORT || 3000
-app.listen(port, () => { console.log(`Server going nuts on port ${port}`); });
+app.listen(port, '0.0.0.0', () => { console.log(`Server going nuts on port ${port}`); });
