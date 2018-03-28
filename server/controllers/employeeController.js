5b637a4dd844af4c01fb2444fd1b39e5d473176d
AJ Larson <aj.larson@devmounta.in>
2018-03-28 12:10:28 -0600

    updated employee return

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -18,27 +18,6 @@ module.exports = {
         const { id, name, phone, email, title } = req.body;
 
         dbInstance.hireEmployee([id, name, phone, email, title])
-            .then(() => res.status(200).send())
-            .catch(() => res.status(500).send());
-
-        // let employee = {
-        //     id: id,
-        //     name: name,
-        //     phone: phone,
-        //     title: title,
-        //     email: email
-        // }
-        // employees.push(employee);
-        // id++;
-        // res.status(200).send(employees);
-    },
-    update: (req, res, next) => {
-        const dbInstance = req.app.get('db');
-        const { params, query } = req
-        console.log(params)
-        console.log(query)
-        if (query.id)
-            dbInstance.updateEmployeeAndID([params.id, query.id, query.name, query.phone, query.email, query.title])
             .then(() => {
                 dbInstance.getEmployees()
                     .then(employees => res.status(200).send(employees))
@@ -47,7 +26,25 @@ module.exports = {
                         res.status(500).send(err)
                     })
             })
-            .catch(() => res.status(500).send())
+            .catch(() => res.status(500).send());
+
+    },
+    update: (req, res, next) => {
+        const dbInstance = req.app.get('db');
+        const { params, query } = req
+        console.log(params)
+        console.log(query)
+        if (query.id)
+            dbInstance.updateEmployeeAndID([params.id, query.id, query.name, query.phone, query.email, query.title])
+                .then(() => {
+                    dbInstance.getEmployees()
+                        .then(employees => res.status(200).send(employees))
+                        .catch(err => {
+                            console.log(err)
+                            res.status(500).send(err)
+                        })
+                })
+                .catch(() => res.status(500).send())
         else
             dbInstance.updateEmployee([params.id, query.name, query.phone, query.email, query.title])
                 .then(() => {
@@ -59,32 +56,23 @@ module.exports = {
                         })
                 })
                 .catch(() => res.status(500).send())
-        // let index = null;
-        // employees.forEach((employee, i) => {
-        //   if(employee.id === Number(req.params.id)) index = i;
-        // })
-        // employees[ index ] = {
-        //   id: employees[ index ].id,
-        //   title: req.body.title || employees[ index ].title,
-        //   phone: req.body.phone || employees[ index ].phone,
-        //   email: req.body.email || employees[ index ].email,
-        //   name: req.body.name || employees[ index ].name
-        // };
-        // res.status(200).send( employees );
+
     },
     delete: (req, res, next) => {
         const dbInstance = req.app.get('db');
         const { params } = req
 
         dbInstance.deleteEmployee([params.id])
-            .then(() => res.status(200).send())
+            .then(() => {
+                dbInstance.getEmployees()
+                    .then(employees => res.status(200).send(employees))
+                    .catch(err => {
+                        console.log(err)
+                        res.status(500).send(err)
+                    })
+            })
             .catch(() => res.status(500).send())
-        // let index = null;
-        // employees.forEach((employee, i) => {
-        //     if (employee.id === Number(req.params.id)) index = i;
-        // })
-        // employees.splice(index, 1)
-        // res.status(200).send(employees);
+
     }
 
 };
